f057c4491e98b5f2b990f1beee49d074016fce4b
天健 王 <OrdinaryDay21@outlook.com>
2025-10-07 23:26:36 +0800

    End of U3

--- API/Program.cs
+++ API/Program.cs
@@ -9,12 +9,15 @@ builder.Services.AddControllers();
 builder.Services.AddDbContext<AppDbContext>(options =>
 {
     options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
-    
+
 });
+builder.Services.AddCors();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+app.UseCors(options => options.AllowAnyHeader().AllowAnyMethod()
+    .WithOrigins("http://localhost:4200", "https://localhost:4200"));
 
 app.MapControllers();
 
