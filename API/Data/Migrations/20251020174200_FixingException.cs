34e1d75b9653df81a6a8b5a4197a3fa2decbef20
天健 王 <OrdinaryDay21@outlook.com>
2025-10-26 21:41:29 +0800

    attach

---
+++ API/Data/Migrations/20251020174200_FixingException.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace API.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixingException : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "DateOfBirsh",
+                table: "Members",
+                newName: "DateOfBirth");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "DateOfBirth",
+                table: "Members",
+                newName: "DateOfBirsh");
+        }
+    }
+}
